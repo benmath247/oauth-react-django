64be98dc25474717972514e0b71fb9907c8a8f01
Ben Math <benjamin.aaron.math@gmail.com>
2025-04-11 16:22:51 -0400

    pokemon fix

--- oauth_example/core/signals.py
+++ oauth_example/core/signals.py
@@ -7,23 +7,20 @@ from django.core.files import File
 import os
 import random
 
-def download_dog_picture():
-    response = requests.get('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'+random.randint(1, 151)+'.png')
-    if response.status_code == 200:
-        data = response.json()
-        if 'message' in data:
-            image_url = data['message']
-            img_response = requests.get(image_url)
-            if img_response.status_code == 200:
-                filename = os.path.basename(image_url)
-                file = BytesIO(img_response.content)
-                return file, filename
+def download_pokemon_picture():
+    pokemon_id = str(random.randint(1, 151))
+    image_url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'+pokemon_id+'.png'
+    img_response = requests.get(image_url)
+    if img_response.status_code == 200:
+        filename = os.path.basename("pokemon_" + pokemon_id + ".png")
+        file = BytesIO(img_response.content)
+        return file, filename
     return None, None
 
 @receiver(post_save, sender=get_user_model())
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        file, filename = download_dog_picture()
+        file, filename = download_pokemon_picture()
         if file and filename:
             instance.profile_picture.save(filename, File(file))
             print(f"Profile picture saved for {instance.username}")
